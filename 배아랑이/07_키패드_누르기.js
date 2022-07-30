90acf363346f2529ddddeb03bd1c9c37510ee1e3
raaang <graytorgb@gmail.com>
2022-07-30 09:23:44 +0900

    [배아랑이] 7 키패드 누르기

---
+++ 배아랑이/07_키패드_누르기.js
@@ -0,0 +1,76 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/67256
+// 45 min
+
+function solution(numbers, hand) {
+	var answer = '';
+
+	// 성공 -> 1.14ms, 30.2MB
+	var keypad = [
+		[1, 2, 3],
+		[4, 5, 6],
+		[7, 8, 9],
+		[10, 11, 12]
+	];
+
+	var left = [3, 0];
+	var right = [3, 2];
+
+	for (let i = 0; i < numbers.length; i++) {
+		if (numbers[i] === 0) {
+			numbers[i] = 11;
+		}
+
+		let tmp = '';
+
+		for (let j = 0; j < keypad.length; j++) {
+			if (numbers[i] === keypad[j][0]) {
+				left = [j, 0];
+				tmp = 'L';
+				break;
+			} else if (numbers[i] === keypad[j][2]) {
+				right = [j, 2];
+				tmp = 'R';
+				break;
+			}
+		}
+
+		if (tmp === '') {
+			const dist_left = Math.abs(parseInt(numbers[i] / 3) - left[0]) + Math.abs(1 - left[1]);
+			const dist_right = Math.abs(parseInt(numbers[i] / 3) - right[0]) + Math.abs(1 - right[1]);
+			// console.log('divide', left, right, dist_left, dist_right);
+
+			if (dist_left === dist_right) {
+				if (hand === 'left') {
+					left = [parseInt(numbers[i] / 3), 1];
+					tmp += 'L';
+				} else {
+					right = [parseInt(numbers[i] / 3), 1];
+					tmp += 'R';
+				}
+			} else if (dist_left < dist_right) {
+				left = [parseInt(numbers[i] / 3), 1];
+				tmp += 'L';
+			} else {
+				right = [parseInt(numbers[i] / 3), 1];
+				tmp += 'R';
+			}
+		}
+
+		answer += tmp;
+	}
+
+	return answer;
+}
+
+// testcase
+let numbers = [
+	[1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5],
+	[7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2],
+	[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+]
+
+let hand = ["right", "left", "right"];
+
+for (let i = 0; i < numbers.length; i++) {
+	console.log(solution(numbers[i], hand[i]));
+}
--- 배아랑이/README.md
+++ 배아랑이/README.md
@@ -8,3 +8,5 @@
 |07/26|4|체육복|Programmers|Level 1|https://school.programmers.co.kr/learn/courses/30/lessons/42862|
 |07/27|5|폰켓몬|Programmers|Level 1|https://school.programmers.co.kr/learn/courses/30/lessons/1845|
 |07/28|6|[1차] 비밀지도|Programmers|Level 1|https://school.programmers.co.kr/learn/courses/30/lessons/17681|
+|07/29|7|키패드 누르기|Programmers|Level 1|https://school.programmers.co.kr/learn/courses/30/lessons/67256|
+
