a48e4e5b68e2eb03f3e3ed3b83e432ed1ef9bdbf
raaang <graytorgb@gmail.com>
2022-07-30 11:22:19 +0900

    [배아랑이] 7 키패드 누르기 주석 추가

--- 배아랑이/07_키패드_누르기.js
+++ 배아랑이/07_키패드_누르기.js
@@ -12,10 +12,12 @@ function solution(numbers, hand) {
 		[10, 11, 12]
 	];
 
-	var left = [3, 0];
-	var right = [3, 2];
+	// 초기 값 행열 넣음
+	var left = [3, 0];		// *
+	var right = [3, 2];		// #
 
 	for (let i = 0; i < numbers.length; i++) {
+		// 0이면 11로 바꿈(keypad 참고)
 		if (numbers[i] === 0) {
 			numbers[i] = 11;
 		}
@@ -34,12 +36,17 @@ function solution(numbers, hand) {
 			}
 		}
 
+		// 중앙값 -> 2, 5, 8, 0
 		if (tmp === '') {
+			// parseInt(5/3) -> 1 -> 행으로 여김
+			// 행의 차이, 열의 차이의 더함
 			const dist_left = Math.abs(parseInt(numbers[i] / 3) - left[0]) + Math.abs(1 - left[1]);
 			const dist_right = Math.abs(parseInt(numbers[i] / 3) - right[0]) + Math.abs(1 - right[1]);
 			// console.log('divide', left, right, dist_left, dist_right);
 
+			// 왼손의 거리, 오른손의 거리 -> 차이를 비교
 			if (dist_left === dist_right) {
+				// 왼손잡이, 오른손잡이에 따라 손 결정
 				if (hand === 'left') {
 					left = [parseInt(numbers[i] / 3), 1];
 					tmp += 'L';
