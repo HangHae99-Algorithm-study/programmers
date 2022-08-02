2e20902268f6927a129bc182aaa2caea920ba175
raaang <graytorgb@gmail.com>
2022-08-02 10:59:51 +0900

    [배아랑이] 9 최댓값과 최솟값

---
+++ 배아랑이/09_최댓값과_최솟값.js
@@ -0,0 +1,22 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12939
+// 5 min
+
+function solution(s) {
+	var answer = '';
+
+	// 성공 -> 0.29ms, 30.2MB
+	var s_num = s.split(' ').map((item) => Number(item)).sort((a, b) => a - b);
+	answer = s_num[0] + ' ' + s_num[s_num.length - 1];
+
+	// 두번째 풀이 -> 0.23ms, 30.4MB
+	// Math.min(), Math.max() 사용
+	var s_num = s.split(' ').map((item) => Number(item));
+	answer = Math.min(...s_num) + ' ' + Math.max(...s_num);
+
+	return answer;
+}
+
+// testcase
+let s = ["1 2 3 4", "-1 -2 -3 -4", "-1 1"]
+
+s.forEach((item) => console.log(solution(item)));
--- 배아랑이/README.md
+++ 배아랑이/README.md
@@ -9,4 +9,5 @@
 |07/27|5|폰켓몬|Programmers|Level 1|https://school.programmers.co.kr/learn/courses/30/lessons/1845|
 |07/28|6|[1차] 비밀지도|Programmers|Level 1|https://school.programmers.co.kr/learn/courses/30/lessons/17681|
 |07/29|7|키패드 누르기|Programmers|Level 1|https://school.programmers.co.kr/learn/courses/30/lessons/67256|
-
+|07/30|8|[1차] 다트 게임|Programmers|Level 1|https://school.programmers.co.kr/learn/courses/30/lessons/17682|
+|08/01|9|최댓값과 최솟값|Programmers|Level 1|https://school.programmers.co.kr/learn/courses/30/lessons/12939|
