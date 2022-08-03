5c469582608722be3e31ad823f244d5edeeb58e8
neo@zillinks.com <keunhey0107@gmail.com>
2022-08-03 09:06:07 +0900

    숫자의 표현

--- 김은혜/숫자의_표현.js
+++ 김은혜/숫자의_표현.js
@@ -1,23 +1,13 @@
-// 1부터 하나씩 더한 수를 더해준다.
-// 더한 수가 n이 되면 멈추고 cnt를 적립한다.
-// 더한 수가 n을 초과하는 값이면 멈추고 다음으로 간다.
-// 2부터 하나씩 더하기 시작한다.
-
 function solution(n) {
-  let answer = 0;
-  let cnt = 0;
+  var answer = 0;
   for (let i = 1; i <= n; i++) {
+    let total = 0;
     for (let j = i; j <= n; j++) {
-      answer += j;
-      if (answer === n) {
-        cnt++;
-        answer = 0;
-        break;
-      } else if (answer > n) {
-        answer = 0;
-        break;
-      }
+      if (total === n) answer++;
+      if (total > n) break;
+      total += j;
     }
   }
-  return cnt;
+  answer = answer + 1;
+  return answer;
 }
