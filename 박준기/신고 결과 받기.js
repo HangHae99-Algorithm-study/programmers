5adaf9df8c777f2ad23d9f51899b9e67905444c6
Jungi Park <byjungy93@gmail.com>
2022-08-31 19:32:31 +0900

    Update 신고 결과 받기.js

--- 박준기/신고 결과 받기.js
+++ 박준기/신고 결과 받기.js
@@ -5,22 +5,35 @@ function solution(id_list, report, k) {
     
     id_list.map((user)=>{
         report_list[user] = []
-    
     })
     
     report.map((user)=>{
     const [user_id, report_id] = user.split(' ')
     if(!report_list[report_id].includes(user_id))
-    {
-    console.log(report_list[report_id].includes(muzi))
-        // 
-
-    // if()
-    // console.log(report_list[report_id])
-    // report_list[report_id].push(user_id)
+    // {
+    report_list[report_id].push(user_id)
         // console.log(user_id, report_id)
-    }  
+    // } 
+    console.log("Checking user ", user)
+    
+    // console.log("Checking user ", user.split(' '))
+    // console.log("Checking report_list ", report_list[report_id])
+    // console.log("Checking user " , user)
     }
-   )   
+   )
+    
+    
+    for(const key in report_list){
+        console.log("Checking key value ", key)
+        // console.log("What is key value ", report_list[key])
+        if(report_list[key].length >= k){
+            report_list[key].map((user)=>{
+                answer[id_list.indexOf(user)] += 1
+                // console.log("Checking user ", user)
+            }) 
+        }
+    }
+    
+    
    return report_list
 }
