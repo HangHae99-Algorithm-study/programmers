ba0017c35efe8142998afbefc8df6f019b59c684
Jungi Park <byjungy93@gmail.com>
2022-09-06 00:41:30 +0900

    [Done] 신고 결과 받기

--- 박준기/신고 결과 받기.js
+++ 박준기/신고 결과 받기.js
@@ -1,32 +1,28 @@
 function solution(id_list, report, k) {
     
-    const answer = new Array(id_list.length)
-    answer.fill(0)
+    const arr = new Array(id_list.length)
+    arr.fill(0)
     
     const report_list = {}
     
-    id_list.map(user => {
+    id_list.map( user => {
         report_list[user] = []
     })
     
-    // working on this logic
-    report.map(user => {
+    report.map( user => {
         [user_id, report_id] = user.split(' ')
         if(!report_list[report_id].includes(user_id)){
-        report_list[report_id].push(user_id)
+            report_list[report_id].push(user_id)   
         }
-        // console.log("user_id", user_id)
-        // console.log("report_id", report_id)
-        // console.log("Checking user",user)
     })
     
-    for(key in report_list){
-        // console.log(key)
-        if(!report_list[key].length >= k){
-            answer[id_list.indexOf(key)] += 1
+    for(const key in report_list){
+        if(report_list[key].length >= k){
+            report_list[key].map(user =>{
+                arr[id_list.indexOf(user)] += 1
+            })
         }
     }
-    
-    
-    return report_list
+   
+    return arr
 }
