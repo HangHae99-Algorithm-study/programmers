2f729207f11f31ec34145e511112544fd43a8bd1
Jungi Park <byjungy93@gmail.com>
2022-10-03 09:46:05 +0900

    Update 001_시저 암호.js

--- 박준기/001_시저 암호.js
+++ 박준기/001_시저 암호.js
@@ -3,25 +3,37 @@ function solution(s, n) {
     var lower = "abcdefghijklmnopqrstuvwxyz";
     var answer= '';
     
+     // let count = 0
      for(var i =0; i <s.length; i++){
          // i = 0,
          // i = 1.
+        
         var text = s[i];
         if(text == ' ') {
             answer += ' '; 
-            // continue;
+            continue;
+            // count += 1
         }
+        
+        // console.log("Check text", text)
          
         const upperOrLower = upper.includes(text) ? upper : lower;
          
-        let index = upperOrLower.indexOf(text) + n;
+//         console.log("Check upperOrLower", typeof upperOrLower)
+         
+//         console.log("Check upperOrLower index",upperOrLower[0])
+         
+        let index = upperOrLower.indexOf(text) + n; 
+      // let beforeIndex = upperOrLower.indexOf(text)
+      // console.log("Check before index", beforeIndex)
       console.log("Checkig index", index)
       console.log("Checking uOL", upperOrLower)
       if (index >= upperOrLower.length) {
         index -= upperOrLower.length;
+       // console.log("Check index here", index)
+       answer += upperOrLower[index]  
         }
      }
     
-    
-    return s;
+    return answer;
 }
