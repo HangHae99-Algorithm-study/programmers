3a9390572f1e905aff2f752aeab716af645ed2c7
Jungi Park <byjungy93@gmail.com>
2022-10-08 17:49:18 +0900

    [Working] 001_시저 암호_d3.js

--- 박준기/001_시저 암호.js
+++ 박준기/001_시저 암호.js
@@ -1,32 +1,24 @@
 function solution(s, n) {
-    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lower = "abcdefghijklmnopqrstuvwxyz";
-    var answer= '';
-    
-     let count = 0;
-     
-     for(var i =0; i <s.length; i++){
-         // i = 0,
-         // i = 1.
-        var text = s[i];
-        if(text == ' ') {
-            // console.log("Check i", i)
-            answer += ' '; 
-            // console.log("Check answer", answer)
-            // continue;
-        }
-         
-          // console.log("Check answer outside of if", answer)
-        // console.log("Check text", text)
-        const upperOrLower = upper.includes(text) ? upper : lower;
-         
-     let index = upperOrLower.indexOf(text) + n; 
+  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lower = 'abcdefghijklmnopqrstuvwxyz';
+  let answer = '';
   
-      if (index >= upperOrLower.length) {
-       
-        index -= upperOrLower.length;
-            }
-       answer += upperOrLower[index];  
-     }
-    return answer;
+  for(let i = 0; i < s.length; i++){
+      
+      let text = s[i];
+      console.log("Check text", text)
+      if(text == ' '){
+         answer += ' ';
+          continue; 
+      }
+      
+      let upperOrLower = (upper.includes(text) ? upper : lower)
+      
+      let index = upperOrLower.indexOf(text) 
+      if(index > upperOrLower)
+  }
+  
+  // console.log('Check ', s.length)
+
+  return answer;
 }
