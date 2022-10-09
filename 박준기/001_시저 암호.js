6a58d7653430b7a90e1bcf275efcb0213e81b225
Jungi Park <byjungy93@gmail.com>
2022-10-09 09:57:41 +0900

    [Finished] 001_시저 암호_d4.js

--- 박준기/001_시저 암호.js
+++ 박준기/001_시저 암호.js
@@ -1,24 +1,41 @@
 function solution(s, n) {
+    
+  // Assigning Uppercase and Lowercase alphabet
   const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const lower = 'abcdefghijklmnopqrstuvwxyz';
+  // Assigning empty string for the answer variab  le
   let answer = '';
   
-  for(let i = 0; i < s.length; i++){
+  // Looping through S input
+  for(let i=0; i < s.length; i++){
       
+      // Assigning each letter of S 
       let text = s[i];
-      console.log("Check text", text)
+      // If there is an empty string, append the empty string in the asnwer 
       if(text == ' '){
-         answer += ' ';
-          continue; 
+          answer += ' ';
+          // Then jumps over one iteration in the loop
+          continue;
       }
       
-      let upperOrLower = (upper.includes(text) ? upper : lower)
+      // If a text is uppercase or lowercase, assign a whole alphabet of it. 
+      const upperOrLower = upper.includes(text)?upper:lower
       
-      let index = upperOrLower.indexOf(text) 
-      if(index > upperOrLower)
+      // Find an index of the text amaong upperOrLower
+      // Add n to the index variable
+      let index = upperOrLower.indexOf(text) + n;
+      
+      // If Index is equal and bigger than index
+      if( index >= upperOrLower.length){
+           // Subtrack and append the upperOrLower.length from the above the index variable
+           index -= upperOrLower.length
+          
+      }
+      
+      // Appending an answer index among upperOrLower
+      answer += upperOrLower[index]
+      
   }
   
-  // console.log('Check ', s.length)
-
-  return answer;
+   return answer;
 }
