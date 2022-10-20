d9747dea91e0b62077f179cd2dbe6c6fc244c6f0
Jungi Park <byjungy93@gmail.com>
2022-10-20 10:42:49 +0900

    [Working] 002_기능개발_d3.js

--- 박준기/002_기능개발.js
+++ 박준기/002_기능개발.js
@@ -1,32 +1,24 @@
 function solution(progresses, speeds) {
-    var answer = [0];
-    let days = progresses.map((progress, index) => 
-    Math.ceil((100 - progress) / speeds[index]))
+    let answer = [0]
+    let days = progresses.map((progress, index) => {
+        return Math.ceil((100-progress) / (speeds[index]))
+    })
+    
     let maxDay = days[0]
     
-    
-    for(let i = 0, j = 0; i< days.length; i++){
-        // 1. i = 0, j = 0 => 7
-        // 2. i = 1, j = 0 => 3
-        // 3. i = 2, j = 0 => 9
-        
-        if(days[i] <= maxDay) {
-        // 1. i = 0, days[0]: 7 <= 7
-        // 2. i = 1, days[1]: 3 <= 7
-        // 3. i = 2, days[2]: 9 <= 7
-            
-            answer[j] += 1;
-            // 1. j= 0, answer[0] = 0 + 1 => answer = [1]
-            // 2. j= 0, answer[0] = 1 + 1 => answer = [2]
-            
-        }else {
-            // 3. i = 2, maxdays = days[2]: 9
-            maxDay = days[i];
-            
-            // 3, j = 1, answer[1] = [2,1]
-            answer[++j] = 1;
+    for(let i =0, j=0; i< days.length; i++){
+                
+        if(days[i] <= maxDay){
+            answer[j] += 1
+        }
+        else{
+            maxDay = answer[i]
+            answer[++j] =1
         }
     }
+     
+    // console.log("Check i", i, "Check j", j)    
     
+
     return answer;
 }
