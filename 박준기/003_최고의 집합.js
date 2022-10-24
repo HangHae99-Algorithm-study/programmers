a987b2bc35e95ee7f09da31ec6b9360b7952be41
Jungi Park <byjungy93@gmail.com>
2022-10-24 20:27:23 +0900

    [Working] 003_최고의 집합_d1

---
+++ 박준기/003_최고의 집합.js
@@ -0,0 +1,19 @@
+function solution(n, s) {
+    var answer = [];
+
+    multiFun(s)
+    
+    return answer;
+}
+            
+function multiFun(s){
+    
+  let midNum = Math.floor(s/2)
+  
+  for( let i = 1; i <= midNum; i++){
+        for( let j = s-i; j <)
+  }
+            
+  console.log("Check midNum", midNum)  
+  console.log("Checking", s)
+}
