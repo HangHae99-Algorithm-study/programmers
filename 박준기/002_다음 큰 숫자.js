7d6f486bd14a3133961b93fb1ab427608b0eaf8b
Jungi Park <byjungy93@gmail.com>
2022-10-28 11:16:22 +0900

    [Working] 002_다음 큰 숫자_d1

---
+++ 박준기/002_다음 큰 숫자.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let oneNum = n.toString(2);
+    // while (true) {
+    //   n++;
+    //   if (n.toString(2).split("1").length === oneNum) {
+    //       return n;
+    //   } 
+    // }
+    console.log("Hey", oneNum)
+}
