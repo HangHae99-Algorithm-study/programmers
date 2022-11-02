cc90924604936d6e321208e02c0e8d187024a276
Jungi Park <byjungy93@gmail.com>
2022-11-02 19:28:28 +0900

    [Finished] 002_피보나치 수_d2

--- 박준기/002_피보나치 수.js
+++ 박준기/002_피보나치 수.js
@@ -1,24 +1,27 @@
 function solution(n) {
-    // i = 2, n = 3, c = 3
-    let a = 0, b = 1, c = n;
-
-    for(let i = 2; i <= n; i++) {
-    // i = 2, c = 0 + 1 => 1
-    //      , a = 1
-    //      , b = 1
-    // i = 3, c = 1 + 1 => 2
-    //      , a = 1
-    //      , b = 2
-    c = (a + b)%1234567 ;
-    a = b;
-    b = c;
-    }
-  
-    // i = 2, return 1
-    // i = 3, return 2
-    // console.log("Hey ", 4 % 3 )
-    // console.log("Checking", ())
-    return c;
     
+    let a= 0, b=1, c=n
+    
+    for(let i = 2; i<=n; i++){
+
+        
+        c = (a + b) % (1234567)
+        // i = 2, c = 0+1 => 1
+        // i = 3, c = 1+1 => 2
+        // i = 4, c = 1+2 => 3
+        // i = 5, c = 2+3 => 5
+       
+        a = b
+        // i = 2, a = 1
+        // i = 3, a = 1
+        // i = 4, a = 2
+     
+        b = c
+        // i = 2, b = 1
+        // i = 3, b = 2
+        // i = 4, b = 3
+    }
+    
+    return c
     
 }
