4c49af84e54e9a8a1ad6c960c9ce3a804df33e1f
Jungi Park <byjungy93@gmail.com>
2022-11-04 14:20:17 +0900

    [Finished] 002_이진 변환 반복하기_d2

--- 박준기/002_이진 변환 반복하기.js
+++ 박준기/002_이진 변환 반복하기.js
@@ -1,19 +1,29 @@
 function solution(s) {
-    let deleteZero =0
-     let count =0
-     // while(s.length!==1){
-         const originLen = s.length
-         s=s.split('').filter(v=>v==='1').join('')
-         
-         console.log('hello',s)
-         
-         // const len = s.length
-         
-         // console.log('check', len)
-         
-     //     deleteZero+=originLen-len
-     //     s=len.toString(2)
-     //     count++
-     // }
-     // return[count,deleteZero]
- }
+    
+    // number of deleted 0
+    let deleteCount = 0;
+    // number of loop until '1'
+    let count = 0;
+    
+    // looping through until s becomes '1'
+    while(s !== '1'){
+        
+        // length of binary number
+        let originLen = s.length
+        
+        // reassign binary number without 0
+        s=s.split('').filter(one => one==='1').join('')
+        
+        // length of binary number without 0
+        let oneLen = s.length
+        
+        // find number of deleted 0 until 1
+        deleteCount += originLen - oneLen
+        // convert string to binary number
+        s= oneLen.toString(2)
+        // number of loop until s become 1
+        count++
+    }
+    
+    return [count,deleteCount]
+}
